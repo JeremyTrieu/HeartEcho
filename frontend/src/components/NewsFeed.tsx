4d2a298c81016c4aa739c4da5c82c4f7f3a39e23
JeremyTrieu <jeremytrieu1612@gmail.com>
2025-11-13 00:16:23 -0800

    fix bugs

--- frontend/src/components/NewsFeed.tsx
+++ frontend/src/components/NewsFeed.tsx
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 import Notifications from "./Notifications";
-import "../styles/Profile.css";
+import "../styles/NewsFeed.css";
 
 interface Comment {
   _id: string;
